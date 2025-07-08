07ab653ec5cee879e5f6f479df3d19b1b13246fd
Sean Gallogly <galloglyst149@gmail.com>
2025-07-08 21:53:09 +0900

    ah before i forget jus like lemme commit ha

--- app/thoughts/[slug]/page.tsx
+++ app/thoughts/[slug]/page.tsx
@@ -1,40 +1,43 @@
-import { getPostSlugs } from '@/lib/posts';
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import { getPostBySlug, getPostSlugs } from '@/lib/posts';
 import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
   return getPostSlugs().map((slug) => ({slug: slug}));
 }
 
-export default function PostPage({ params }: { params: { slug: string } }) {
-  return (
-      <main className="pt-20 px-4 text-gray-800">
-        <article className="pt-20 max-w-3xl mx-auto space-y-8">
-          <header className="space-y-2">
-            <h1 className="text-2xl font-light leading-tight">
-              Article Title Goes Here
-            </h1>
-            <div className="text-xs text-gray-500 uppercase tracking-wide">
-              August 2025
+export const dynamic = 'force-static';
+
+interface Props {
+  params: {
+    slug: string
+  }
+}
+
+export default function PostPage({ params }: Props ) {
+  const { slug } = params;
+
+  try {
+    const post = getPostBySlug(slug);
+    return (
+        <main className="pt-20 px-4 text-gray-800">
+          <article className="pt-20 max-w-3xl mx-auto space-y-8">
+            <header className="space-y-2">
+              <h1 className="text-2xl font-light leading-tight">
+                {post.frontmatter.title}
+              </h1>
+              <div className="text-xs text-gray-500 uppercase tracking-wide">
+                {post.frontmatter.date}
+              </div>
+            </header>
+            
+            <div className="prose prose-sm sm:prose-base prose-neutral">
+              <MDXRemote source={post.content} />
             </div>
-          </header>
-  
-          <section className="prose prose-sm sm:prose-base prose-neutral">
-            <p>
-              This is the opening paragraph. It should feel light, easy to read,
-              and have a comfortable line height. Whitespace is doing the heavy lifting.
-            </p>
-  
-            <p>
-              When you want to emphasize something, use short, clean lines and don't overcrowd.
-            </p>
-  
-            <h2 className="text-2xl font-light leading-tight">Subhead</h2>
-  
-            <p>
-              Even more content here, nice and relaxed. Let your content breathe.
-            </p>
-          </section>
-        </article>
-      </main>
-  );
+          </article>
+        </main>
+    );
+  } catch (error) {
+    notFound();
+  }
 }
